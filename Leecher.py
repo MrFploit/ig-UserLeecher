d48c03ecfc8c2193424fef7b6a2f367bb6a9012c
it4min <51884521+it4min@users.noreply.github.com>
2020-01-30 10:05:44 +0330

    Create Leecher.py

---
+++ Leecher.py
@@ -0,0 +1,53 @@
+# Code By it4min
+# t.me/it4min
+# t.me/LinuxH
+from instabot import Bot
+import os, getpass
+
+logo = '''
+\033[0;35m
+            ──▄█████████████████████████▄──
+            ▄█▀░█░█░█░░░░░░░░░░░░░░░░░░░▀█▄
+            █░░░█░█░█░░░░░░░░░░░░░░█████░░█
+            █░░░█░█░█░░░░░░░░░░░░░░█████░░█
+            █░░░░░░░░░▄▄▄█████▄▄▄░░░░░░░░░█
+            ███████████▀▀░░░░░▀▀███████████
+            █░░░░░░░██░░▄█████▄░░██░░░░░░░█
+            █░░░░░░░██░██▀░░░▀██░██░░░░░░░█
+            █░░░░░░░██░██░░░░░██░██░░░░░░░█
+            █░░░░░░░██░██▄░░░▄██░██░░░░░░░█
+            █░░░░░░░██▄░▀█████▀░▄██░░░░░░░█
+            █░░░░░░░░▀██▄▄░░░▄▄██▀░░░░░░░░█
+            █░░░░░░░░░░▀▀█████▀▀░░░░░░░░░░█
+            █░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█
+            ▀█▄░░░░░░░░░░░░░░░░░░░░░░░░░▄█▀
+            ──▀█████████████████████████▀──
+\033[0;34m
+           +---  Instagram User Leecher ---+ \033[0;32m
+           t.me/LinuxH
+  ### Login ###
+'''
+
+os.system("clear")
+print (logo)
+
+user = input("Username: ")
+passwd = getpass.getpass("Password: ")
+print
+account = input("Account to extract user: ")
+
+bot = Bot()
+bot.login(username=user, password=passwd, ask_for_code=True)
+user_id = bot.get_user_id_from_username(account)
+followers = bot.get_user_followers(user_id)
+
+f = open("usernames.lst", "w")
+
+for user_id in followers:
+    username = bot.get_username_from_user_id(user_id)
+    print (username)
+    f.write(username+'\n')
+
+f.close()
+
+os.system("rm -rf *log *txt config *json *.checkpoint")
